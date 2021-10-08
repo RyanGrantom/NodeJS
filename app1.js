9a90f91268e9a4355d146973e31cf173c28c766e
Ryan Grantom <Grantom.Ryan@gmail.com>
2021-10-07 19:13:18 -0500

    Changed Folder Structure

--- app1.js
+++
@@ -1,7 +0,0 @@
-console.log(process);
-for(let x=0; x<10000; x++){
-    console.log(x);
-    if(x==3000){
-        process.exit();
-    }
-}
--- mod1.js
+++
@@ -1,5 +0,0 @@
-const test1 = require('./test1');
-
-console.log(test1)
-const myName = `${test1.person.first} ${test1.person.last}`
-console.log(myName);
