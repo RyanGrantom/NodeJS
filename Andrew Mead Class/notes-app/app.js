2c82a6c4dcb0bd302153692217efc1cd324c2a5f
Ryan Grantomm <grantom.ryan@gmail.com>
2021-11-06 11:18:41 -0500

    Restructuring

--- Andrew Mead Class/notes-app/app.js
+++ Andrew Mead Class/notes-app/app.js
@@ -1,9 +1,53 @@
-const chalk = require('chalk');
-const notes = require('./notes');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes')
 
-const msg = notes.getNotes();
-console.log(msg);
+//Customize Yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.green.bold(`success`));
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ', argv.title)
+    }
+})
 
+//create remove command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log('Listing all of the notes')
+    }
+})
 
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+//create delete command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse()
